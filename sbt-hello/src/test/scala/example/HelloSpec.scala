8aaa7d058c33b16698b4a448c20f889da98f10aa
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2018-10-22 12:53:29 +0100

    SBT Hello

---
+++ sbt-hello/src/test/scala/example/HelloSpec.scala
@@ -0,0 +1,10 @@
+package example
+
+import org.scalatest._
+
+class HelloSpec extends FunSuite with DiagrammedAssertions {
+  test("Hello should start with H") {
+    // Hello, as opposed to hello
+    assert("Hello".startsWith("H"))
+  }
+}
