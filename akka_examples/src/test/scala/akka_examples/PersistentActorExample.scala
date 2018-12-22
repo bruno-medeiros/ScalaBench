3c0fc2a636b2d129143fdedecd765d0a752b6a20
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2018-12-22 12:14:34 -0100

    actors

---
+++ akka_examples/src/test/scala/akka_examples/PersistentActorExample.scala
@@ -0,0 +1,62 @@
+package akka_examples
+
+import akka.actor.Props
+import akka.persistence.PersistentActor
+import akka.testkit.TestProbe
+
+object CounterActor {
+
+  sealed trait Op
+
+  case class Increment(amount: Int) extends Op
+  case class Decrement(amount: Int) extends Op
+
+  case class OpMsg(op: Op)
+}
+
+class CounterActor extends PersistentActor {
+  import CounterActor._
+
+  var counter: Int = 0
+
+  println("CounterActor create")
+
+  override def persistenceId: String = "counter-actor"
+
+  override def receiveRecover: Receive = {
+    case op: Op =>
+      println(s"Recovering: $op")
+      updateState(op)
+//    case SnapshotOffer(_, _) => ???
+  }
+
+  override def receiveCommand: Receive = {
+    ???
+  }
+
+  private def updateState(op: Op): Unit = {
+    println(s"updating state: $op")
+    op match {
+      case Increment(amount) =>
+        counter += amount
+      case Decrement(amount) =>
+        counter -= amount
+    }
+  }
+}
+
+
+class PersistentActorExample extends AkkaTest {
+  import CounterActor._
+
+  "Counter Actor" in {
+    val probe = TestProbe()
+    val counterActor = system.actorOf(Props[CounterActor], "counter-actor-p")
+    probe.send(counterActor, Increment(10))
+
+    probe.send(counterActor, OpMsg(Increment(10)))
+
+    //    counterActor ! Increment(10)
+//    counterActor ! Increment(20)
+  }
+}
---
+++ akka_examples/src/test/scala/akka_examples/RoutingExample.scala
@@ -0,0 +1,27 @@
+package akka_examples
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.routing.{FromConfig, RoundRobinGroup}
+import akka_examples.RoutingExample.MyWorker
+
+object RoutingExample {
+  class MyWorker extends Actor {
+    override def receive: Receive = {
+      case "foo" => println("foo")
+    }
+  }
+}
+
+class RoutingExample extends AkkaTest {
+
+  "Example Router with children" in {
+    val router1: ActorRef =
+      system.actorOf(FromConfig.props(Props[MyWorker]), "router1")
+  }
+
+  "Example Router with actor selection" in {
+    val paths = List("/user/workers/w1", "/user/workers/w2", "/user/workers/w3")
+    val router4: ActorRef =
+      system.actorOf(RoundRobinGroup(paths).props(), "router4")
+  }
+}
