c3dfef4521b2205e6030cfa19cad790430f753eb
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2019-05-07 16:15:41 +0100

    akka streams hello

--- akka_examples/src/main/scala/akka_examples/AkkaStreamsHello.scala
+++ akka_examples/src/main/scala/akka_examples/AkkaStreamsHello.scala
@@ -3,10 +3,17 @@ package akka_examples
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{FileIO, Flow, Keep, Sink, Source}
-import akka.stream.{ActorMaterializer, IOResult}
+//import akka.stream.scaladsl.
+import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
+import akka.stream.ActorMaterializer
+import akka.stream.IOResult
 import akka.util.ByteString
-import akka.{Done, NotUsed}
+import akka.Done
+import akka.NotUsed
 
 import scala.concurrent.Future
 
@@ -43,7 +50,6 @@ object AkkaStreamsFactorialApp extends App {
       .map(num ⇒ ByteString(s"$num\n"))
       .runWith(FileIO.toPath(Paths.get("factorials.txt")))
 
-
   implicit val ec = system.dispatcher
   printSourceFt.zipWith(factorialsFileWriteFt)((_, _)).onComplete(_ ⇒ system.terminate())
 
@@ -52,6 +58,7 @@ object AkkaStreamsFactorialApp extends App {
 
 object Misc {
   def lineSink(filename: String): Sink[String, Future[IOResult]] = {
+
     val sink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(Paths.get(filename))
     val flow: Flow[String, String, NotUsed] = Flow[String]
     flow
---
+++ scala_examples/src/main/scala/java_examples/JavaExamples.java
@@ -0,0 +1,9 @@
+package java_examples;
+
+public class JavaExamples {
+    public static void main(String[] args) {
+        var name = "World5";
+
+        System.out.println("Hello " + name);
+    }
+}
