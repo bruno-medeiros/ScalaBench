87fa91765ce2e197a1f685e82520be0c88f85905
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2020-02-24 12:59:53 +0000

    Doobie and monad conversion

---
+++ scala_examples/src/test/scala/cats_examples/DoobieExample.scala
@@ -0,0 +1,76 @@
+package cats_examples
+
+import scala.concurrent.{ ExecutionContext, ExecutionContextExecutor }
+
+import cats.effect.{ Blocker, IO }
+import doobie._
+import doobie.implicits._
+import doobie.util.transactor.Transactor
+import doobie.util.transactor.Transactor.Aux
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{ FreeSpec, Matchers }
+
+case class Foo(num: Int, name: String)
+
+class DoobieExample extends FreeSpec with Matchers with ScalaFutures {
+
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+
+  // We need a ContextShift[IO] before we can construct a Transactor[IO]. The passed ExecutionContext
+  // is where nonblocking operations will be executed. For testing here we're using a synchronous EC.
+  implicit val cs = IO.contextShift(ec)
+
+  val useH2 = true
+  val (driver, dburl) =
+    if (useH2)
+      ("org.h2.Driver", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+    else
+      ("org.postgresql.Driver", "jdbc:postgresql:postgres")
+
+  // A transactor that gets connections from java.sql.DriverManager and executes blocking operations
+  // on an our synchronous EC. See the chapter on connection handling for more info.
+  val xa: Aux[IO, Unit] = Transactor.fromDriverManager[IO](
+    driver, // driver classname
+    dburl, // connect URL (driver-specific)
+    "postgres", // user
+    "", // password
+    Blocker.liftExecutionContext(ec),
+  )
+
+  "example" in {
+
+    sql"select 123, 'abc'; SELECT 123, 'abcdef'"
+      .query[(Int, String)]
+      .option
+      .transact(xa)
+      .unsafeRunSync
+      .foreach(println)
+
+    sql"select 123, 'abc'"
+      .query[Foo]
+      .to[Vector]
+      .transact(xa)
+      .unsafeRunSync
+      .foreach(println)
+
+    sql"select 123, 'abc'"
+      .query[Foo]
+      .to[Vector]
+      .map[String](e => throw new Exception("boom"))
+      .transact(xa)
+      .unsafeToFuture()
+      .failed
+      .futureValue
+      .getMessage should equal("boom")
+
+  }
+
+  "transact example" in {
+
+    sql"SET TRANSACTION ISOLATION LEVEL READ COMMITTED; SELECT 42".update.run
+      .transact(xa)
+      .unsafeRunSync
+
+  }
+
+}
---
+++ scala_examples/src/test/scala/examples/monads/MonadConversions.scala
@@ -0,0 +1,96 @@
+package examples.monads
+
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success, Try }
+
+import cats.{ Monad, MonadError }
+import org.scalatest.FreeSpec
+import org.scalatest.concurrent.ScalaFutures
+
+//noinspection ScalaUnusedSymbol
+class MonadConversions extends FreeSpec with ScalaFutures {
+
+  "Future and throws " in {
+    implicit val ec = ExecutionContext.global
+
+    Future.successful(123).map(throwException)
+      .failed
+      .futureValue
+      .getMessage
+      .assertEqual("Failed")
+
+    Future.failed(new Exception("xxx"))
+      .recover{ case e => throwException(111)}
+      .failed.futureValue
+      .getMessage
+      .assertEqual("Failed")
+
+
+    assertThrows[Exception] {
+      // Fails because it's the first for clause
+      for {
+        _ <- Future.successful(throwException(123))
+      } yield ()
+    }
+
+    {
+      for {
+        _ <- Future.successful(123)
+        // if the exception is throw in second clause tho, it works:
+        _ <- Future.successful(throwException(123))
+      } yield ()
+    }
+  }
+
+  def throwException(num: Int): String = {
+    throw new Exception("Failed")
+  }
+  implicit class AssertEqualsOps[T](obj: T) {
+    def assertEqual(other: T) = assert(obj == other)
+  }
+
+  "Monad and MonadError" in {
+    implicit val ec = ExecutionContext.global
+    val fme: MonadError[Future, Throwable] = cats.implicits.catsStdInstancesForFuture
+
+    val result: Future[String] = fme.unit
+      .map[String](e => throw new Exception("Failed Future"))
+    result.failed.futureValue.getMessage.assertEqual("Failed Future")
+
+    val optMonad: Monad[Option] = cats.implicits.catsStdInstancesForOption
+
+    // Option is not a MonadError, and so cannot handle exceptions
+    assertThrows[Exception] {
+      val result: Option[String] = optMonad.unit
+        .map[String](e => throw new Exception("Failed Future"))
+    }
+  }
+
+  // ----------------------
+
+  "Convert one Monad to another " in {
+
+    def convert(option: Option[Int]): Try[String] = {
+      option match {
+        case Some(value) => Success(value.toString)
+        case None => Failure(new Exception("value is missing!"))
+      }
+    }
+
+    def convert2(option: Option[Int]): Try[String] = {
+      option
+        .map(value => Success(value.toString))
+        .getOrElse(Failure(new Exception("value is missing!")))
+    }
+  }
+
+  "Convert Monad to another - with MonadError" in {
+
+    def convert[F[_]](option: Option[Int])(implicit me: MonadError[F, Throwable]): F[String] = {
+      option
+        .map(value => me.pure(value.toString))
+        .getOrElse(me.raiseError(new Exception("value is missing!")))
+    }
+  }
+
+}
